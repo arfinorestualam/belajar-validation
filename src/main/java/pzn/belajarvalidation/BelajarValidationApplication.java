92317e780463f50dd2a0331a23414c35b711f31f
arfinorestualam <arfino.alam@gmail.com>
2024-11-13 10:47:21 +0700

    first

---
+++ src/main/java/pzn/belajarvalidation/BelajarValidationApplication.java
@@ -0,0 +1,13 @@
+package pzn.belajarvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarValidationApplication.class, args);
+    }
+
+}
